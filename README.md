cda18c59d959a6e560b176cac4a4666cad187802
20000coinbe <kimdks12@gmail.com>
2021-06-08 14:29:10 +0900

    puppeteer 예제

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Front-End 연습 토이 프로젝트
 
-### 크롤링(Crawling)과 스크래핑(Scraping)
+크롤링(Crawling)과 스크래핑(Scraping)
+
+---
+
+## 1-3. Puppeteer
+
+```js
+// 예제
+```
+
+---
--- pptr.js
+++ pptr.js
@@ -1,1 +1,10 @@
-// ed
+const puppeteer = require("puppeteer");
+
+// IIFE : 즉시실행함수
+async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  await page.screenshot({ path: "example.png" });
+  await browser.close();
+};
